c2b5971393492e51289b18350bc61d57c6515def
Deni Alfaro <denilorenaalfaro@hotmail.com>
2018-08-16 08:33:46 -0700

    Constructores

---
+++ practicaObjeto/Contacto.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrácticaObjetos
+{
+    class Contacto
+    {
+        public string Nombre { get; set; }
+        public string TelefonoCelular { get; set; }
+        public string TelefonoCasa { get; set; }
+        public string Direccion { get; set; }
+        public string CorreoElectronico { get; set; }
+
+        public Contacto()
+        {
+            Nombre = "Nuevo Contacto";
+            TelefonoCasa = "(000) 000 00 00";
+        }
+
+        public Contacto(string nombre)
+        {
+            Nombre = nombre;
+        }
+        //No marca error porque hay varios strings y no uno como en el anterior, no tiene que tener el mismo numero de strings)
+        public Contacto(string nombre, string telefonoCelular, string telefonoCasa, string direccion, string correoElectronico)
+        {
+            Nombre = nombre;
+            TelefonoCelular = telefonoCelular;
+            TelefonoCasa = telefonoCasa;
+            Direccion = direccion;
+            CorreoElectronico = correoElectronico;
+        }
+    }
+}
--- practicaObjeto/Program.cs
+++ practicaObjeto/Program.cs
@@ -10,22 +10,26 @@ namespace PrácticaObjetos
     {
         static void Main(string[] args)
         {
-            Persona persona1 = new Persona();
-            persona1.nombre = "Juan";
-            persona1.apellidos = "Perez";
-            persona1.edad = 20;
+            Contacto contacto1 = new Contacto();
 
-            Console.WriteLine(persona1.nombre + " " + persona1.apellidos + " tiene " + persona1.edad + " años");
+            contacto1.Nombre = "Pedro Lopez";
+            contacto1.TelefonoCasa = "(644) 115 -2514";
+            contacto1.Direccion = "Guerrero 456";
 
-            persona1.cumplirAños();
-            Console.WriteLine("Ahora " + persona1.nombre + " tiene " + persona1.edad + " años");
+            Console.WriteLine("Nombre: " + contacto1.Nombre);
+            Console.WriteLine("Telefono Casa: " + contacto1.TelefonoCasa);
 
-            persona1.altura = 1.5f; //el double se convirtió en float (double ocupa mas memoria que float)
-            Console.WriteLine(persona1.nombre + " mide " + persona1.altura);
+            Contacto contacto2 = new Contacto("Juan Perez");
+            Console.WriteLine("Contacto 2");
+            Console.WriteLine("Nombre: " + contacto2.Nombre);
 
-            persona1.crecer(0.12f);
-            Console.WriteLine(persona1.nombre + " ahora mide: " + persona1.altura);
-
+            Contacto contacto3 = new Contacto("Maria Navarro", "123456789", "987654321", "5 de febrero 895", "maria@correo.com");
+            Console.WriteLine("Contacto 3");
+            Console.WriteLine("Nombre: " + contacto3.Nombre);
+            Console.WriteLine("Telefono celular: " + contacto3.TelefonoCelular);
+            Console.WriteLine("Telefono casa: " + contacto3.TelefonoCasa);
+            Console.WriteLine("Direccion: " + contacto3.Direccion);
+            Console.WriteLine("Correo Electronico: " + contacto3.CorreoElectronico);
             Console.ReadLine();
         }
     }
